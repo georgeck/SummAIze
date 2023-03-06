ddefec17c7d3d4445075690583c21dc71a16aea7
George Chiramattel <georgeck@gmail.com>
2023-03-06 11:42:15 -0800

    Added Umami Cloud analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
-import { Analytics } from '@vercel/analytics/react';
+import {Analytics} from '@vercel/analytics/react';
+import Script from 'next/script'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
     return (
         <>
             <Component {...pageProps} />
-            <Analytics />
+            <Analytics/>
+            <Script strategy="afterInteractive" src="https://analytics.umami.is/script.js"
+                    data-website-id="6fa86d7f-8c94-4fcb-94c4-5e9a08f7e2f2" />
         </>
     );
 }
