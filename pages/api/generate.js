77153a6389742b5c771e837be331e8897d9d3ab6
George Chiramattel <georgeck@gmail.com>
2023-03-10 22:46:40 -0800

    Updated engine version

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,9 +8,9 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-export const config = {
-    runtime: "edge",
-};
+// export const config = {
+//     runtime: "edge",
+// };
 
 const openai = new OpenAIApi(configuration);
 
